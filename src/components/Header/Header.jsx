80521faf4c59df7e291b1936293e3b40112568c3
Benjamin <benjaminbandasavanh@gmail.com>
2024-01-12 14:37:06 +0100

    mise a jour mediaqueries pour le header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,41 @@
 import '../../style/style.scss';
 import { NavLink } from 'react-router-dom';
 import Logo from '../../Logo/Logo.png';
+import { useState } from 'react';
 
 export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+
     return (
         <header>
-        <nav className='nav'>
-            <div className="nav__logo">
-            <NavLink to="/" exact>
-            <img className='Logo' src={Logo} alt="logo" />
-            </NavLink>
-            </div>
-            <div className="nav__links">
-        <NavLink to="/" exact>
-        Home
-        </NavLink>
-        <NavLink to="/portfolio">
-        Portfolio
-        </NavLink>
-        <NavLink to="/competences">
-        Skills
-        </NavLink>
-        <NavLink to="/contact">
-        Contact
-        </NavLink>
+
+<nav className="navbar">
+        <div className="navbar__logo">
+          <img className='Logo' src={Logo} alt="logo" />
         </div>
-        </nav>
+        <button className="navbar__toggle" id="navbar-toggle" onClick={toggleMenu}>
+          <span className="burger">&#9776;</span>
+        </button>
+        <ul id="navbar-container" className={`navbar__container ${isMenuOpen ? 'active' : ''}`}>
+          <li className="navbar__item navbar__item--active">
+           <NavLink to='/'>Home</NavLink>
+          </li>
+          <li className="navbar__item">
+            <NavLink to='/competences'>Skills</NavLink>
+          </li>
+          <li className="navbar__item">
+          <NavLink to='/portfolio'>Portfolio</NavLink>
+          </li>
+          <li className="navbar__item">
+          <NavLink to='/contact'>Contact</NavLink>
+          </li>
+        </ul>
+      </nav>
         </header>
     );
     }
